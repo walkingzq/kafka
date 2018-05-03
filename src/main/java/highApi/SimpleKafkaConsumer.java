ad274034fce4ac258c44482acb12c404ec932dc4
walkingzq <walkingzq@163.com>
2018-05-03 15:58:06 +0800

    kafka0.10.1的consumer api简单使用

--- src/main/java/highApi/SimpleKafkaConsumer.java
+++ src/main/java/highApi/SimpleKafkaConsumer.java
@@ -3,6 +3,7 @@ package highApi;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,7 +22,7 @@ public class SimpleKafkaConsumer {
     public static void main(String[] args){
         String kafka_server = "10.87.52.135:9092,10.87.52.134:9092,10.87.52.158:9092";
 //        String kafka_zk = "10.87.52.135:2181,10.87.52.134:2181,10.87.52.158:2181/kafka-0.10.1.1";
-        String topic = "basetest";
+        String topic = "test";
         Properties props = new Properties();
         props.put("bootstrap.servers", kafka_server);
         props.put("group.id", "kafka_consumer");
@@ -32,6 +33,8 @@ public class SimpleKafkaConsumer {
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
         consumer.subscribe(Arrays.asList(topic));
         Map<Long, Long> countMap = new HashMap<>();
+//        TopicPartition partition = new TopicPartition(topic, 1);
+//        consumer.seek(partition, 2);//消费指定分区指定位置
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(100);
             for (ConsumerRecord<String, String> record : records){
